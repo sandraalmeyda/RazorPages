4589b55ae5c1039f1065e99a9fb46535b7001b1a
hishamco <hishamco_2007@yahoo.com>
2016-08-26 17:20:57 +0300

    Using @page routing in the sample

--- samples/RazorPages.Samples.Web/Data/Customer.cs
+++ samples/RazorPages.Samples.Web/Data/Customer.cs
@@ -1,7 +1,6 @@
-﻿
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace RazorPages.Samples.Web
+namespace RazorPages.Samples.Web.Data
 {
     public class Customer
     {
---
+++ samples/RazorPages.Samples.Web/Pages/EditCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPages.Samples.Web.Data;
+
+namespace RazorPages.Samples.Web.Pages
+{
+    public class EditCustomer : Page
+    {
+        public EditCustomer(AppDbContext db)
+        {
+            Db = db;
+        }
+
+        public AppDbContext Db { get; }
+
+        public Data.Customer GetCustomer(int id)
+        {
+            var customer = Db.Customers.Where(c => c.Id == id).SingleOrDefault();
+
+            return customer;
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            var currentCustomer = Db.Customers.Single(c => c.Id == customer.Id);
+            
+            currentCustomer.Name = customer.Name;
+            Db.SaveChanges();
+        }
+    }
+}
