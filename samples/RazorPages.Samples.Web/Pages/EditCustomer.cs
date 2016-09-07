04788218a4f9a2fbbba0598ac090f00f2475a38a
Ryan Nowak <nowakra@gmail.com>
2016-09-07 09:44:26 -0700

    Use code-behind and tempdata

--- samples/RazorPages.Samples.Web/Pages/EditCustomer.cs
+++ samples/RazorPages.Samples.Web/Pages/EditCustomer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPages.Samples.Web.Data;
 
@@ -13,7 +14,39 @@ namespace RazorPages.Samples.Web.Pages
 
         public AppDbContext Db { get; }
 
-        public Data.Customer GetCustomer(int id)
+        public Customer CurrentCustomer { get; set; }
+
+        public string SuccessMessage { get; set; }
+
+        public bool ShowSuccessMessage => !string.IsNullOrEmpty(SuccessMessage);
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public bool ShowErrorMessage => !string.IsNullOrEmpty(ErrorMessage);
+
+        public IActionResult OnGet(int id)
+        {
+            CurrentCustomer = GetCustomer(id);
+            return View(CurrentCustomer);
+        }
+
+        public IActionResult OnPost(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                UpdateCustomer(customer);
+                SuccessMessage = $"Customer {customer.Id} successfully edited!";
+                return Redirect("/");
+            }
+
+            // Model errors, populate customer list and show errors
+            CurrentCustomer = GetCustomer(customer.Id);
+            ErrorMessage = "Please correct the errors and try again";
+            return View();
+        }
+
+        public Customer GetCustomer(int id)
         {
             var customer = Db.Customers.Where(c => c.Id == id).SingleOrDefault();
 
